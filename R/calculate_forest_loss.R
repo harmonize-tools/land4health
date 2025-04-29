2849e2704c702efd1d3c123c1b23cceb873a0fc4
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2025-04-28 22:45:16 -0500

    updated

--- R/calculate_forest_loss.R
+++ R/calculate_forest_loss.R
@@ -11,8 +11,35 @@
 #' `r lifecycle::badge('stable')`
 #'
 #' @export
-calculate_forest_loss <- \(to, from, region, fun, progress = FALSE, sf = TRUE) {
+calculate_forest_loss <- \(from, to, region, fun, progress = FALSE, sf = TRUE) {
+
+  if (to < from) {
+    cli::cli_abort(c(
+      "!" = "Invalid dates: {.field from} = {from}, {.field to} = {to}.",
+      "x" = "The {.field to} date must be later than the {.field from} date."
+    ))
+  }
+
+  start_date <- as.integer(
+    substr(
+      regmatches(to,regexpr("\\d{4}", to)),
+      start = 3,
+      stop = 4)
+  )
+
+  end_date <- as.integer(
+    substr(
+      regmatches(from,regexpr("\\d{4}", from)),
+      start = 3,
+      stop = 4)
+  )
+
+  range_date <- start_date:end_date
+
+  hansen_id <- .internal_data$hansen
+
   sf_box <- region |> sf::st_bbox()
+
   r <- rgeedim::gd_bbox(
     xmin = sf_box["xmin"],
     xmax = sf_box["xmax"],
@@ -20,11 +47,14 @@ calculate_forest_loss <- \(to, from, region, fun, progress = FALSE, sf = TRUE) {
     ymax = sf_box["ymax"]
   )
 
-  x <- rgeedim::gd_image_from_id(
-    x = getOption(
-      x = "land4health",
-      default = .internal_data$hansen)
-    )
-
+  x <- hansen_id |>
+    rgeedim::gd_image_from_id() |>
+    rgeedim::gd_download(
+      filename = tempfile(fileext = ".tif"),
+      region = sf_box,
+      bands = list("lossyear"),
+      scale = scale,
+      overwrite = TRUE,
+      silent = FALSE)
 
 }
