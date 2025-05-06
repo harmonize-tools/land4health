53d31d7d908e695e06cf33811f8430d291d49303
Antony Marcos Barja Ingaruca <mvcs_dgppvu_ambi@vivienda.gob.pe>
2025-05-06 09:41:48 -0500

    rgee and sf > land4health

---
+++ R/attach.R
@@ -0,0 +1,45 @@
+# cribbed from https://github.com/tidyverse/tidyverse/blob/main/R/attach.R
+# Uses MIT license
+core <- c(
+  "rgee",
+  "sf"
+)
+
+core_unloaded <- function() {
+  search <- paste0("package:", core)
+  core[!search %in% search()]
+}
+
+# Attach the package from the same package library it was
+same_library <- function(pkg) {
+  loc <- if (pkg %in% loadedNamespaces()) dirname(getNamespaceInfo(pkg, "path"))
+  library(pkg, lib.loc = loc, character.only = TRUE, warn.conflicts = FALSE)
+}
+
+# attaches all the packages from core that are not loaded
+land4health_attach <- function() {
+  to_load <- core_unloaded()
+  suppressPackageStartupMessages(
+    lapply(to_load, same_library)
+  )
+
+  invisible(to_load)
+}
+
+#' List all land4health packages
+#'
+#' @param include_self default `TRUE`. Includes the "land4health" package name in the
+#'   resultant character vector.
+#' @returns A character vector of package names included in the "land4health" meta-package.
+#' @export
+#' @examples
+#' land4health_packages()
+#'
+# https://github.com/tidyverse/tidyverse/blob/main/R/utils.R
+land4health_packages <- function(include_self = TRUE) {
+  pkgs <- core
+  if (include_self) {
+    pkgs <- c(pkgs, "land4health")
+  }
+  pkgs
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive() && !isTRUE(getOption("land4health.shownWelcome"))) {
 
-    name.project <- cli::bg_red(cli::style_bold(cli::col_white("Harmonize Project")))
-
+    # Block 1 : Presentation of Harmonize --------------------------------------
     cli::cli_h1("Welcome to land4health")
-    cli::cli_alert_info("{.emph A tool of {.href [{name.project}](https://www.harmonize-tools.org/)} for calculate and extract Remote Sensing Metrics for Spatial Health Analysis}")
-    cli::cli_alert_info("{.emph Currently,`land4health` supports metrics related to the following categories:}")
+    cli::cli_text("{.emph A tool of {.href [{.pkg Harmonize Project}](https://www.harmonize-tools.org/)} to calculate and extract Remote Sensing Metrics for Spatial Health Analysis.}")
+    cli::cli_text("{.emph Currently,`land4health` supports metrics related to the following categories:}")
 
+    # Block 2 : Metrics available ----------------------------------------------
     category_data <- get_metrics_summary()
     list_categories <- c(if (is.data.frame(category_data) && "category" %in% names(category_data)) {
       category_data$category[1:min(4, nrow(category_data))] |> as.vector()
@@ -15,9 +15,33 @@
     for (category in list_categories) {
       cli::cli_li(category)
     }
-
-    cli::cli_alert_info("{.emph For more information about metrics, please use the `get_metrics_metadata()` function.}")
-
+    # Block 3: Additional help
+    cli::cli_text("{.emph For more information about metrics, please use the `get_metrics_metadata()` function.}")
     options(land4health.shownWelcome = TRUE)
+  }
+
+    # Block 4: Packages backend of land4health ----------------------------------
+    cli::cli_h1(text = "Attaching core land4health packages")
+    attached <- land4health_attach()
+    if (length(attached) > 0) {
+      pkg_ul(attached)
+    }
+}
+
+# cribbed from https://github.com/R-ArcGIS/arcgis/blob/main/R/zzz.R
+pkg_ul <- function(pkgs) {
+  pkg_versions <- vapply(
+    pkgs,
+    function(.x) as.character(utils::packageVersion(.x)),
+    character(1)
+  )
+
+  for (i in seq_along(pkgs)) {
+    pkg <- pkgs[i]
+    ver <- pkg_versions[i]
+    cli::cli_inform(
+      c(">" = "{.pkg {pkg}} v{ver}"),
+      class = "packageStartupMessage"
+    )
   }
 }
