a7a228f386485015ac8cfa54099f736ffabe941d
Antony Marcos Barja Ingaruca <mvcs_dgppvu_ambi@vivienda.gob.pe>
2025-07-04 13:24:50 -0500

    up

--- R/zzz.R
+++ R/zzz.R
@@ -1,50 +1,37 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive() && !isTRUE(getOption("land4health.shownWelcome"))) {
-
     # Block 1 : Presentation of Harmonize --------------------------------------
-    withCallingHandlers({
-      cli::cli_h1("Welcome to land4health")
-      cli::cli_text("{.emph A tool of {.href [{.pkg Harmonize Project}](https://www.harmonize-tools.org/)} to calculate and extract Remote Sensing Metrics for Spatial Health Analysis. Currently,{.code land4health} supports metrics in the following categories:}")
+    cli::cli_h1("Welcome to land4health")
+    cli::cli_text("{.emph A tool of {.href [{.pkg Harmonize Project}](https://www.harmonize-tools.org/)} to calculate and extract Remote Sensing Metrics for Spatial Health Analysis.}")
+    cli::cli_text("{.emph Currently,`land4health` supports metrics related to the following categories:}")
 
-      # Block 2 : Metrics available ----------------------------------------------
-      providers_count <- get_data() |>
-        subset(select = "category") |>
-        table() |>
-        as.data.frame() |>
-        tidyr::as_tibble()
-      names(providers_count) <- c("category", "metrics_counts")
-      category_data <- providers_count
-      list_categories <- c(if (is.data.frame(category_data) && "category" %in% names(category_data)) {
-        category_data$category[1:min(3, nrow(category_data))] |> as.vector()
-      } else {
-        character()
-      }, "and more!")
+    # Block 2 : Metrics available ----------------------------------------------
+    providers_count <- get_data() |>
+      subset(select = "category") |>
+      table() |>
+      as.data.frame() |>
+      tidyr::as_tibble()
+    names(providers_count) <- c("category", "metrics_counts")
+    category_data <- providers_count
+    list_categories <- c(if (is.data.frame(category_data) && "category" %in% names(category_data)) {
+      category_data$category[1:min(4, nrow(category_data))] |> as.vector()
+    } else {
+      character()
+    }, "and more!")
 
-      for (category in list_categories) {
-        cli::cli_li(category)
-      }
-
-
-      cli::cli_text("{.emph For a complete list of available metrics, use the {.code l4h_list_metrics()} function.}")
-    },
-    message = function(m) {
-      packageStartupMessage(m$message)
-      invokeRestart("muffleMessage")
-    })
-
+    for (category in list_categories) {
+      cli::cli_li(category)
+    }
+    # Block 3: Additional help
+    cli::cli_text("{.emph For more information about metrics, please use the `l4h_list_metrics()` function.}")
     options(land4health.shownWelcome = TRUE)
   }
 
   # Block 4: Packages backend of land4health ----------------------------------
-  withCallingHandlers({
-    cli::cli_h1("")
-    cli::cli_inform("Attaching core {.pkg land4health} packages:")
-  },
-  message = function(m) {
-    packageStartupMessage(m$message)
-    invokeRestart("muffleMessage")
-  })
-
+  cli::cli_inform(
+    "Attaching core {.pkg land4health} packages:",
+    class = "packageStartupMessage"
+  )
   attached <- land4health_attach()
   if (length(attached) > 0) {
     pkg_ul(attached)
@@ -58,6 +45,7 @@ pkg_ul <- function(pkgs) {
     function(.x) as.character(utils::packageVersion(.x)),
     character(1)
   )
+
   for (i in seq_along(pkgs)) {
     pkg <- pkgs[i]
     ver <- pkg_versions[i]
